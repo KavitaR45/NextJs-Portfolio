6f856873dc02999d99d9ef6d8da008649c5e44c9
@Kavita2599 <kavita99@netart.io>
2024-10-24 23:29:26 +0530

    my project timeline added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import TerminalComponent from "@/components/terminalComponent";
 import TechStack from "@/components/techStack"
+import Image from "next/image";
+import { Timeline } from "@/components/ui/timeline";
 export default function Home() {
   return (
     <>
@@ -25,10 +27,76 @@ export default function Home() {
         <TechStack />
       </div>
       <div className="flex flex-col justify-center gap-8 h-full relative items-center p-10 flex-wrap">
-        <h1 className="text-4xl sm:text-5xl font-bold">Tech Stack</h1>
+        <h1 className="text-4xl sm:text-5xl font-bold">My Projects</h1>
         <p className="text-xl w-full lg:w-3/5 text-center">Copy paste the most trending components and use them in your websites without having to worry about styling and animations.</p>
-        <TechStack />
+        <div className="w-full">
+          <Timeline data={data} />
+        </div>
       </div>
     </>
   );
 }
+
+const data = [
+  {
+    title: "2024",
+    content: (
+      <div>
+        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+          Built and launched Aceternity UI and Aceternity UI Pro from scratch
+        </p>
+        <div className="grid grid-cols-2 gap-4">
+          
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "Early 2023",
+    content: (
+      <div>
+        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+          I usually run out of copy, but when I see content this big, I try to
+          integrate lorem ipsum.
+        </p>
+        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+          Lorem ipsum is for people who are too lazy to write copy. But we are
+          not. Here are some more example of beautiful designs I built.
+        </p>
+        <div className="grid grid-cols-2 gap-4">
+         
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "Changelog",
+    content: (
+      <div>
+        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
+          Deployed 5 new components on Aceternity today
+        </p>
+        <div className="mb-8">
+          <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+            ✅ Card grid component
+          </div>
+          <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+            ✅ Startup template Aceternity
+          </div>
+          <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+            ✅ Random file upload lol
+          </div>
+          <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+            ✅ Himesh Reshammiya Music CD
+          </div>
+          <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+            ✅ Salman Bhai Fan Club registrations open
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+         
+        </div>
+      </div>
+    ),
+  },
+];
--- components/techStack.tsx
+++ components/techStack.tsx
@@ -12,49 +12,49 @@ export default function TechStack() {
       }
     >
       <MagicCard
-        className="cursor-pointer h-[8  0px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
+        className="cursor-pointer h-[80px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
         gradientColor={theme === "dark" ? "#262626" : "#ECDFCC"}
       >
         <Image alt='nextjs-tech' src={`/image/tech/${theme == "dark" ? "nextjs-light":"nextjs"}.png`} width='80' height='80' />
       </MagicCard>
       <MagicCard
-        className="cursor-pointer h-[8  0px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
+        className="cursor-pointer h-[80px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
         gradientColor={theme === "dark" ? "#262626" : "#ECDFCC"}
       >
         <Image alt='javascript-tech' src="/image/tech/javascript.png" width='48' height='48' />
       </MagicCard>
       <MagicCard
-        className="cursor-pointer h-[8  0px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
+        className="cursor-pointer h-[80px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
         gradientColor={theme === "dark" ? "#262626" : "#ECDFCC"}
       >
         <Image alt='react-tech' src="/image/tech/react.png" width='48' height='48' />
       </MagicCard>
       <MagicCard
-        className="cursor-pointer h-[8  0px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
+        className="cursor-pointer h-[80px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
         gradientColor={theme === "dark" ? "#262626" : "#ECDFCC"}
       >
         <Image alt='erp-tech' src="/image/tech/erpnext.png" width='100' height='100' />
       </MagicCard>
       <MagicCard
-        className="cursor-pointer h-[8  0px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
+        className="cursor-pointer h-[80px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
         gradientColor={theme === "dark" ? "#262626" : "#ECDFCC"}
       >
         <Image alt='wordpress-tech' src="/image/tech/wordpress.png" width='48' height='48' />
       </MagicCard>
       <MagicCard
-        className="cursor-pointer h-[8  0px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
+        className="cursor-pointer h-[80px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
         gradientColor={theme === "dark" ? "#262626" : "#ECDFCC"}
       >
         <Image alt='html-tech' src="/image/tech/html.png" width='48' height='48' />
       </MagicCard>
       <MagicCard
-        className="cursor-pointer h-[8  0px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
+        className="cursor-pointer h-[80px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
         gradientColor={theme === "dark" ? "#262626" : "#ECDFCC"}
       >
         <Image alt='woocommerce-tech' src="/image/tech/woocommerce.png" width='48' height='48' />
       </MagicCard>
       <MagicCard
-        className="cursor-pointer h-[8  0px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
+        className="cursor-pointer h-[80px] lg:w-1/5 md:w-2/5 w-full flex-col items-center justify-center whitespace-nowrap text-4xl"
         gradientColor={theme === "dark" ? "#262626" : "#ECDFCC"}
       >
         <Image alt='strapi-tech' src={`/image/tech/${theme == "dark" ? "strapi-light":"strapi"}.png`} width='80' height='80' />
