79e1b5eff159c3cce20e327435b431ddc0590e7f
@Kavita2599 <kavita99@netart.io>
2024-10-25 22:25:05 +0530

    3 project data addded

--- app/page.tsx
+++ app/page.tsx
@@ -39,64 +39,51 @@ export default function Home() {
 
 const data = [
   {
-    title: "2024",
+    title: "Sync Media",
     content: (
       <div>
-        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          Built and launched Aceternity UI and Aceternity UI Pro from scratch
+        <p className="text-neutral-800 dark:text-neutral-200 text-md md:text-lg font-normal mb-8">
+        A website designed for an advertising agency, featuring captivating animations that respond to user interaction. Parallax and reveal effects are incorporated to enhance the visual experience, while Three.js is utilized to create a wave animation in the banner.
         </p>
         <div className="grid grid-cols-2 gap-4">
-          
+          <Image src="/image/project/sync-1.png" height={635} width={1365} alt='sync-image'/>
+          <Image src="/image/project/sync-2.png" height={635} width={1365} alt='sync-image'/>
+          <Image src="/image/project/sync-3.png" height={635} width={1365} alt='sync-image'/>
+          <Image src="/image/project/sync-4.png" height={635} width={1365} alt='sync-image'/>
         </div>
       </div>
     ),
   },
   {
-    title: "Early 2023",
+    title: "Glastone",
     content: (
       <div>
-        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          I usually run out of copy, but when I see content this big, I try to
-          integrate lorem ipsum.
-        </p>
-        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          Lorem ipsum is for people who are too lazy to write copy. But we are
-          not. Here are some more example of beautiful designs I built.
-        </p>
-        <div className="grid grid-cols-2 gap-4">
-         
-        </div>
+      <p className="text-neutral-800 dark:text-neutral-200 text-md md:text-lg font-normal mb-8">
+      A website designed for an advertising agency, featuring captivating animations that respond to user interaction. Parallax and reveal effects are incorporated to enhance the visual experience, while Three.js is utilized to create a wave animation in the banner.
+      </p>
+      <div className="grid grid-cols-2 gap-4">
+        <Image src="/image/project/glastone-1.png" height={635} width={1365} alt='glastone-image'/>
+        <Image src="/image/project/glastone-2.png" height={635} width={1365} alt='glastone-image'/>
+        <Image src="/image/project/glastone-3.png" height={635} width={1365} alt='glastone-image'/>
+        <Image src="/image/project/glastone-4.png" height={635} width={1365} alt='glastone-image'/>
       </div>
+    </div>
     ),
   },
   {
-    title: "Changelog",
+    title: "WVStor",
     content: (
       <div>
-        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-          Deployed 5 new components on Aceternity today
-        </p>
-        <div className="mb-8">
-          <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-            ✅ Card grid component
-          </div>
-          <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-            ✅ Startup template Aceternity
-          </div>
-          <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-            ✅ Random file upload lol
-          </div>
-          <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-            ✅ Himesh Reshammiya Music CD
-          </div>
-          <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-            ✅ Salman Bhai Fan Club registrations open
-          </div>
-        </div>
-        <div className="grid grid-cols-2 gap-4">
-         
-        </div>
+      <p className="text-neutral-800 dark:text-neutral-200 text-md md:text-lg font-normal mb-8">
+      A website designed for an advertising agency, featuring captivating animations that respond to user interaction. Parallax and reveal effects are incorporated to enhance the visual experience, while Three.js is utilized to create a wave animation in the banner.
+      </p>
+      <div className="grid grid-cols-2 gap-4">
+        <Image src="/image/project/wvstor-1.png" height={635} width={1365} alt='wvstor-image'/>
+        <Image src="/image/project/wvstor-2.png" height={635} width={1365} alt='wvstor-image'/>
+        <Image src="/image/project/wvstor-3.png" height={635} width={1365} alt='wvstor-image'/>
+        <Image src="/image/project/wvstor-4.png" height={635} width={1365} alt='wvstor-image'/>
       </div>
+    </div>
     ),
   },
 ];
